1aafce3961c70ebb32b5c1cf20f7c71b2d144f67
DanyeleSales <danyelesales@hotmail.com>
2018-11-09 11:19:31 -0200

    consumindo api google, imprimindo imagem de fundo

--- 07-exercicio/src/js/index.js
+++ 07-exercicio/src/js/index.js
@@ -4,15 +4,17 @@ import credentials from './credentials'
 import google_image_search from './resources/google'
 import musicbrainz_artists_search from './resources/musicbrainz';
 
+
+
 // manipulação do dom
 const input = document.querySelector("input");
 const div = document.querySelector("content");
 const form = document.querySelector("form");
-
+const img = document.getElementById("bg");
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
-   google_image_search(input.value, div)
+   google_image_search(input.value, img)
        
     
 })
--- 07-exercicio/src/js/resources/google.js
+++ 07-exercicio/src/js/resources/google.js
@@ -1,14 +1,16 @@
 import axios from 'axios'
 import credentials from '../credentials'
-function google_image_search(input, div){
-    axios.get(`https://www.googleapis.com/customsearch/v1?key=${credentials.google.key}cx=${credentials.google.cx}&q=${input}&searchType=image&imgSize=huge`)
-        .then(function (response) {
-           
-        console.log('sucesso',response.data);
-        })
-        .catch(function (error) {
-        console.log('error', error);
-        });
+function google_image_search(input, img) {
+  axios.get(`https://www.googleapis.com/customsearch/v1?key=${credentials.google.key}&cx=${credentials.google.cx}&q=${input}&searchType=image&imgSize=huge`)
+    .then(function (response) {
+      console.log(response.data.items[0].link);
+      img.src = response.data.items[0].link;
+      img.style.opacity= 1;      
+      console.log('sucesso',response.data);
+    })
+    .catch(function (error) {
+      console.log('error', error);
+    });
 }
 
 
